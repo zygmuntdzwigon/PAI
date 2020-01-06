20f8210845bd8041f21b3bcdc671666bf22183d3
zygmuntdzwigon <zygmuntdxwigon@gmail.com>
2020-01-06 17:46:31 +0100

    backend

---
+++ my_e_wallet/debt/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+#
+# class Debt(models.Model):
+#    title = models.CharField(max_length=50)
+#    description = models.TextField()
+#    date = models.DateTimeField(auto_now_add=True)
+#    amount = models.FloatField()
+#    creditor = models.ForeignKey()
+     
---
+++ paienv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zyga/Repos/PAI/paienv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
